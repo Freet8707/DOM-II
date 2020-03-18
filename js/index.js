1291586833623d4748ff0b0f88ad64800ce08514
JeremyFreetly <freet8707@gmail.com>
2020-03-17 23:05:13 -0500

    1st event listener added to fun-bus img

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+const imageZoom = document.querySelector("img")
+
+imageZoom.addEventListener("mouseenter", () => {
+    imageZoom.style.transform = "scale(1.2)"
+    imageZoom.style.transition = "all 0.3s"
+})
+
+imageZoom.addEventListener("mouseleave", () => {
+    imageZoom.style.transform = "scale(1)"
+    imageZoom.style.transition = "all 0.3s"
+})
