fff1b63f3eedaef6fdc63373f683ead633a25490
JeremyFreetly <freet8707@gmail.com>
2020-03-18 00:46:41 -0500

    display toggle created on 3rd image

--- js/index.js
+++ js/index.js
@@ -21,8 +21,8 @@ navLinks.forEach((element) =>{
     })
 })
 
-const h2Click = document.querySelectorAll("h2") 
-h2Click.forEach((element) => {
+const h2Selector = document.querySelectorAll("h2") 
+h2Selector.forEach((element) => {
     element.addEventListener("click", () => {
         element.style.color = "#1df900"
         element.style.fontSize = "3.5rem"
@@ -36,9 +36,15 @@ firstP.addEventListener("dblclick", () => {
     firstP.style.color = "#a123ea"
 })
 
+
+
 const imgList = document.querySelectorAll("img")
 imgList[1].addEventListener("mouseover", (element) => {
     imgList[1].src = "https://picsum.photos/200/200"
     imgList[1].style.width = "100%"
     imgList[1].style.height = "auto"
+})
+
+imgList[2].addEventListener("click", () => {
+    imgList[2].classList.toggle("jeremys-class")
 })
