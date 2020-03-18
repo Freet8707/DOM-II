8de118201a0198066afe67cfbedaacc4fae9980c
JeremyFreetly <freet8707@gmail.com>
2020-03-17 23:24:22 -0500

    mouse enter and leave event listeners added to nav links

--- js/index.js
+++ js/index.js
@@ -10,3 +10,13 @@ imageZoom.addEventListener("mouseleave", () => {
     imageZoom.style.transform = "scale(1)"
     imageZoom.style.transition = "all 0.3s"
 })
+
+const navLinks = document.querySelectorAll(".nav-link")
+navLinks.forEach((element) =>{
+    element.addEventListener("mouseenter", () => {
+        element.style.color = "#A7C4CB";
+    })
+    element.addEventListener("mouseleave", () => {
+        element.style.color = "#212529"
+    })
+})
